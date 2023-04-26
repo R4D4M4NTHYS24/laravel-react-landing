bd385f700a26b82c9ba0d5164c9c2abba2cb2a42
Camilo Restrepo <camilorg2017@gmail.com>
2023-04-26 15:36:48 -0500

    create menu and video functionalities

--- resources/js/Index.jsx
+++ resources/js/Index.jsx
@@ -1,82 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import { createRoot } from "react-dom/client";
-import "../../public/css/general.css";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+
 import "../../public/css/style.css";
-import "../../public/css/queries.css";
+
+const root = createRoot(document.getElementById("indexreact"));
 
 export default function Index() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
+
     return (
         <div>
-            <header className="header">
-                <a href="#">
+            <header>
+                <div className="sticky">
                     <img className="logo" alt="logo" src="/img/logo.svg" />
-                </a>
-                <nav className="main-nav">
-                    <ul className="main-nav-list">
-                        <li>
-                            <a className="main-nav-link" href="#">
-                                HOME
-                            </a>
-                        </li>
-                        <li>
-                            <a className="main-nav-link" href="#characters">
-                                THE CREW
-                            </a>
-                        </li>
-                        <li>
-                            <a className="main-nav-link" href="#project">
-                                EVENTS
-                            </a>
-                        </li>
-                        <li>
-                            <a className="main-nav-link" href="#project">
-                                WHAT'S NEW
-                            </a>
-                        </li>
-                        <li>
-                            <a className="main-nav-link" href="#project">
-                                VIRTUAL OFFICE
-                            </a>
-                        </li>
-                        <li>
-                            <a className="main-nav-link" href="#contact">
-                                CONTACT
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                    <nav ref={navRef}>
+                        <a href="#">HOME</a>
 
-                <button className="btn-mobile-nav">
-                    <ion-icon
-                        className="icon-mobile-nav"
-                        name="menu-outline"
-                    ></ion-icon>
-                    <ion-icon
-                        className="icon-mobile-nav"
-                        name="close-outline"
-                    ></ion-icon>
-                </button>
-            </header>
+                        <a href="#characters">THE CREW</a>
 
-            <section className="section-hero">
-                <div className="hero">
-                    <video autoPlay loop muted>
-                        <source
-                            media="(min-width: 769px)"
-                            src="/img/video/Final2.mp4"
-                        />
-                        <source
-                            media="(max-width: 768px)"
-                            src="/img/video/Final.mp4"
-                        />
-                    </video>
+                        <a href="#project">EVENTS</a>
+
+                        <a href="#project">WHAT'S NEW</a>
+
+                        <a href="#project">VIRTUAL OFFICE</a>
+
+                        <a href="#contact">CONTACT</a>
+                        <button className="nav-btn" onClick={showNavbar}>
+                            <FaTimes />
+                        </button>
+                    </nav>
+                    <button className="nav-btn" onClick={showNavbar}>
+                        <FaBars />
+                    </button>
                 </div>
+            </header>
+            <section className="section-hero">
+                <video autoPlay loop muted>
+                    <source
+                        media="(min-width: 769px)"
+                        src="/img/video/Final2.mp4"
+                    />
+                    <source
+                        media="(max-width: 768px)"
+                        src="/img/video/Final.mp4"
+                    />
+                </video>
             </section>
         </div>
     );
 }
 
-if (document.getElementById("indexreact")) {
-    const root = createRoot(document.getElementById("indexreact"));
-    root.render(<Index />);
-}
+root.render(<Index />);
